301255abe2afa605714195bd451e0166dc910417
Fabio Bove <fabio.bove.dr@gmail.com>
2023-04-15 23:21:46 +0200

    update code

--- test.py
+++ test.py
@@ -1,8 +1,16 @@
 import yaml
-from GitHubSecretsInitializer import GitHubSecretsInitializer
+import json
+import requests
+import base64
+import requests
+import json
+import requests
+import json
 
 
-def test():
+
+
+if __name__ == "__main__":
     # Load the oauth_settings.yml file
     stream = open('oauth_settings.yml', 'r')
     settings = yaml.load(stream, yaml.SafeLoader)
@@ -13,14 +21,33 @@ def test():
     github_token = settings['github_token']
     secrets_file_path = "secrets.json"
 
-    # Initialize the GitHubSecrets object
-    github_secrets = GitHubSecretsUpdater(repo_owner, repo_name, github_token)
 
-    try:
-        github_secrets.initialize_secrets(secrets_file_path)
-    except Exception as e:
-        print(f"Error initializing secrets: {e}")
+    # Construct API URL
+    base_url = f"https://api.github.com/repos/{repo_owner}/{repo_name}/actions/secrets/public-key"
+    headers = {"Authorization": f"Bearer {github_token}"}
 
+    # Send request to GitHub API to get public key ID
+    response = requests.get(base_url, headers=headers)
+    response_json = response.json()
+    public_key_id = response_json["key_id"]
 
-if __name__ == '__main__':
-    test()
+    print(f"Public key ID for {repo_owner}/{repo_name}: {public_key_id}")
+
+    # Read the secrets from the JSON file
+    with open(secrets_file_path, "r") as secrets_file:
+        secrets = json.load(secrets_file)
+
+    # Loop through the secrets and create/update them in GitHub
+    for secret_name, secret_value in secrets.items():
+        url = f"https://api.github.com/repos/{repo_owner}/{repo_name}/actions/secrets/{secret_name}"
+        headers = {"Authorization": f"Bearer {github_token}"}
+        data = {
+            "encrypted_value": base64.b64encode(secret_value.encode("utf-8")).decode("utf-8"),
+             "key_id": public_key_id
+        }
+        response = requests.put(url, headers=headers, json=data)
+        if response.status_code == 201:
+            print(f"Successfully created/updated secret '{secret_name}'")
+        else:
+            print(f"Error creating/updating secret '{secret_name}': {response.text}")
+
