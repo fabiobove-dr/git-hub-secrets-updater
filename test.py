a65a616d1703f0be5b4faf4fef28b90bc77ae938
Fabio Bove <fabio.bove.dr@gmail.com>
2023-04-15 22:08:20 +0200

    updated gitignore and test module

--- test.py
+++ test.py
@@ -1,13 +1,17 @@
+import yaml
 from GitHubSecretsUpdater import GitHubSecretsUpdater
 
-def test():
 
-    
+def test():
+    # Load the oauth_settings.yml file
+    stream = open('oauth_settings.yml', 'r')
+    settings = yaml.load(stream, yaml.SafeLoader)
+
     # Define the necessary variables
-    repo_owner = "your-repo-owner"
-    repo_name = "your-repo-name"
-    github_token = "your-github-token"
-    secrets_file_path = "/path/to/secrets/file.json"
+    repo_owner = settings[]
+    repo_name = settings[]
+    github_token = settings[]
+    secrets_file_path = "secrets.json"
 
     # Initialize the GitHubSecrets object
     github_secrets = GitHubSecretsUpdater(repo_owner, repo_name, github_token, secrets_file_path)
